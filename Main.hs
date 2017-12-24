a993e63c2784b24750652a1404412c640a4c3859
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-12-24 20:37:20 +0700

    Init

---
+++ Main.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Test.HUnit
+
+import Text.Printf
+import Data.Text as T
+
+numberToCurrency n = T.pack (printf "$%.2f" n)
+
+main = runTestTT $ test $ [
+  "simple" ~: "$5.12" ~=? numberToCurrency(5.12 :: Double)
+  , "decimal rounding" ~: "$5.12" ~=? numberToCurrency(5.117 :: Double)]
