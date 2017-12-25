66657f760b914f75452381701971c4795089e94f
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-12-25 15:25:29 +0700

    Negative numbers

--- Main.hs
+++ Main.hs
@@ -9,11 +9,13 @@ commafy n = T.reverse $  intercalate "," (chunksOf 3 (T.reverse n))
 commafyIntegerPart n = T.concat [(commafy integerPart), ".", fractionalPart]
         where (integerPart: fractionalPart: _) = splitOn "." n
 
-numberToCurrency :: (PrintfArg a) => a -> Text
-numberToCurrency n = commafyIntegerPart $ T.pack (printf "$%.2f" n)
+numberToCurrency :: (Ord a, Num a, PrintfArg a) => a -> Text
+numberToCurrency n = T.concat [(if isNegative then "-" else ""), commafyIntegerPart $ T.pack (printf "$%.2f" (abs n))]
+        where isNegative = n < 0
 
 main = runTestTT $ test $ [
   "simple" ~: "$5.12" ~=? numberToCurrency(5.12 :: Double)
   , "significant digits" ~: "$5.00" ~=? numberToCurrency(5 :: Double)
   , "decimal rounding" ~: "$5.12" ~=? numberToCurrency(5.117 :: Double)
-  , "commas" ~: "$1,234,567,890.50" ~=? numberToCurrency(1234567890.50 :: Double)]
+  , "commas" ~: "$1,234,567,890.50" ~=? numberToCurrency(1234567890.50 :: Double)
+  , "negative" ~: "-$1,234,567,890.50" ~=? numberToCurrency(-1234567890.50 :: Double)]
