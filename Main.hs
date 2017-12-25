754a3f00662dc8edf5957dfccef3d96e45dbd310
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-12-25 13:18:04 +0700

    Commas

--- Main.hs
+++ Main.hs
@@ -5,9 +5,15 @@ import Test.HUnit
 import Text.Printf
 import Data.Text as T
 
-numberToCurrency n = T.pack (printf "$%.2f" n)
+commafy n = T.reverse $  intercalate "," (chunksOf 3 (T.reverse n))
+commafyIntegerPart n = T.concat [(commafy integerPart), ".", fractionalPart]
+        where (integerPart: fractionalPart: _) = splitOn "." n
+
+numberToCurrency :: (PrintfArg a) => a -> Text
+numberToCurrency n = commafyIntegerPart $ T.pack (printf "$%.2f" n)
 
 main = runTestTT $ test $ [
   "simple" ~: "$5.12" ~=? numberToCurrency(5.12 :: Double)
   , "significant digits" ~: "$5.00" ~=? numberToCurrency(5 :: Double)
-  , "decimal rounding" ~: "$5.12" ~=? numberToCurrency(5.117 :: Double)]
+  , "decimal rounding" ~: "$5.12" ~=? numberToCurrency(5.117 :: Double)
+  , "commas" ~: "$1,234,567,890.50" ~=? numberToCurrency(1234567890.50 :: Double)]
