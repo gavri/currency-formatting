37ee3f5bba0bbf08ff0998adb1ba453fb1875913
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-12-25 11:53:02 +0700

    Test significant digits without rounding

--- Main.hs
+++ Main.hs
@@ -9,4 +9,5 @@ numberToCurrency n = T.pack (printf "$%.2f" n)
 
 main = runTestTT $ test $ [
   "simple" ~: "$5.12" ~=? numberToCurrency(5.12 :: Double)
+  , "significant digits" ~: "$5.00" ~=? numberToCurrency(5 :: Double)
   , "decimal rounding" ~: "$5.12" ~=? numberToCurrency(5.117 :: Double)]
