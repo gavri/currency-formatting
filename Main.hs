821fb9b75da76a53b47d49c3c5c312a2f3fa3acd
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-12-26 18:53:42 +0700

    Zero precision

--- Main.hs
+++ Main.hs
@@ -6,8 +6,10 @@ import Text.Printf
 import Data.Text as T
 
 commafy n = T.reverse $  intercalate "," (chunksOf 3 (T.reverse n))
-commafyIntegerPart n = T.concat [(commafy integerPart), ".", fractionalPart]
-        where (integerPart: fractionalPart: _) = splitOn "." n
+commafyIntegerPart n = case (Prelude.length components) of
+  1 -> commafy (Prelude.head components)
+  otherwise -> T.concat [(commafy (Prelude.head components)), ".", (Prelude.last components)]
+  where components = splitOn "." n
 
 data CurrencyFormattingOptions = CurrencyFormattingOptions {
   precision :: Int
@@ -25,7 +27,7 @@ numberToCurrency n = numberToCurrencyRaw n defaultCurrencyFormattingOptions
 numberToCurrencyWithOptions n options = numberToCurrencyRaw n options
 
 sign n = if isNegative then "-" else ""
-        where isNegative = n < 0
+  where isNegative = n < 0
 
 main = runTestTT $ test $ [
   "simple" ~: "$5.12" ~=? numberToCurrency(5.12 :: Double)
@@ -33,4 +35,5 @@ main = runTestTT $ test $ [
   , "decimal rounding" ~: "$5.12" ~=? numberToCurrency(5.117 :: Double)
   , "commas" ~: "$1,234,567,890.50" ~=? numberToCurrency(1234567890.50 :: Double)
   , "negative" ~: "-$1,234,567,890.50" ~=? numberToCurrency(-1234567890.50 :: Double)
-  , "precision" ~: "-$1,234,567,890.507" ~=? numberToCurrencyWithOptions (-1234567890.5067 :: Double) (defaultCurrencyFormattingOptions {precision = 3})]
+  , "precision" ~: "-$1,234,567,890.507" ~=? numberToCurrencyWithOptions (-1234567890.5067 :: Double) (defaultCurrencyFormattingOptions {precision = 3})
+  , "zero precision" ~: "-$1,234,567,891" ~=? numberToCurrencyWithOptions (-1234567890.5067 :: Double) (defaultCurrencyFormattingOptions {precision = 0})]
