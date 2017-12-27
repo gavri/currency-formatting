71e9be3563df06f6644f80d91debf1c851e0ac52
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-12-27 17:06:49 +0700

    Parameterize separator

--- Main.hs
+++ Main.hs
@@ -6,25 +6,27 @@ import Text.Printf
 import Data.Text as T
 
 commafy n delimiter = T.reverse $  intercalate delimiter (chunksOf 3 (T.reverse n))
-commafyIntegerPart n delimiter = case (Prelude.length components) of
+commafyIntegerPart n delimiter separator = case (Prelude.length components) of
   1 -> commafy (Prelude.head components) delimiter
-  otherwise -> T.concat [(commafy (Prelude.head components) delimiter), ".", (Prelude.last components)]
+  otherwise -> T.concat [(commafy (Prelude.head components) delimiter), separator, (Prelude.last components)]
   where components = splitOn "." n
 
 data CurrencyFormattingOptions = CurrencyFormattingOptions {
   precision :: Int
   , unit :: Text
   , delimiter :: Text
+  , separator :: Text
 }
 
 defaultCurrencyFormattingOptions = CurrencyFormattingOptions {
   precision = 2
   , unit = "$"
   , delimiter = ","
+  , separator = "."
 }
 
 numberToCurrencyRaw :: (Ord a, Num a, PrintfArg a) => a -> CurrencyFormattingOptions -> Text
-numberToCurrencyRaw n options  = T.concat [sign n, (unit options), commafyIntegerPart (T.pack (printf ("%." ++ (show (precision options)) ++ "f") (abs n))) (delimiter options)]
+numberToCurrencyRaw n options  = T.concat [sign n, (unit options), commafyIntegerPart (T.pack (printf ("%." ++ (show (precision options)) ++ "f") (abs n))) (delimiter options) (separator options)]
 
 numberToCurrency n = numberToCurrencyRaw n defaultCurrencyFormattingOptions
 
@@ -43,4 +45,5 @@ main = runTestTT $ test $ [
   , "zero precision" ~: "-$1,234,567,891" ~=? numberToCurrencyWithOptions (-1234567890.5067 :: Double) (defaultCurrencyFormattingOptions {precision = 0})
   , "unit" ~: "£5.12" ~=? numberToCurrencyWithOptions (5.12 :: Double) defaultCurrencyFormattingOptions {unit = "£"}
   , "delimiter" ~: "$1;234;567;890.50" ~=? numberToCurrencyWithOptions (1234567890.50 :: Double) defaultCurrencyFormattingOptions {delimiter = ";"}
-  , "delimiter and zero precision" ~: "$1;234;567;891" ~=? numberToCurrencyWithOptions (1234567890.5067 :: Double) defaultCurrencyFormattingOptions {delimiter = ";", precision = 0}]
+  , "delimiter and zero precision" ~: "$1;234;567;891" ~=? numberToCurrencyWithOptions (1234567890.5067 :: Double) defaultCurrencyFormattingOptions {delimiter = ";", precision = 0}
+  , "separator" ~: "$1,234,567,890;50" ~=? numberToCurrencyWithOptions (1234567890.50 :: Double) defaultCurrencyFormattingOptions {separator = ";"}]
