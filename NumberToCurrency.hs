180f6e494186e7809ada5dbfe7f68ddb7ea181ca
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-12-28 12:35:33 +0700

    Move out tests

---
+++ NumberToCurrency.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module NumberToCurrency (convert, convertWithOptions, defaultCurrencyFormattingOptions,
+                        precision, unit, delimiter, separator, format, negativeFormat) where
+
+import Text.Printf
+import Data.Text as T hiding (head, last, length)
+
+convertWithOptions num options  = (replace "%u" u . replace "%n" n) chosenFormat
+  where n = commafyIntegerPart (pack (printf magnitudeFormat magnitude)) (delimiter options) (separator options)
+        u = unit options
+        chosenFormat = if (num < 0) then (negativeFormat options) else (format options)
+        magnitudeFormat = "%." ++ (show (precision options)) ++ "f"
+        magnitude = abs num
+
+convert n = convertWithOptions n defaultCurrencyFormattingOptions
+
+commafyIntegerPart n delimiter separator = if hasFractionalPart
+                                             then commafiedIntegerPart `append` separator `append` fractionalPart
+                                             else commafiedIntegerPart
+  where commafiedIntegerPart = commafy integerPart delimiter
+        integerPart = head components
+        fractionalPart = last components
+        hasFractionalPart = length components /= 1
+        components = splitOn "." n
+
+commafy n delimiter = T.reverse $  intercalate delimiter (chunksOf 3 (T.reverse n))
+
+data CurrencyFormattingOptions = CurrencyFormattingOptions {
+  precision :: Int
+  , unit :: Text
+  , delimiter :: Text
+  , separator :: Text
+  , format :: Text
+  , negativeFormat :: Text
+}
+
+defaultCurrencyFormattingOptions = CurrencyFormattingOptions {
+  precision = 2
+  , unit = "$"
+  , delimiter = ","
+  , separator = "."
+  , format = "%u%n"
+  , negativeFormat = "-%u%n"
+}
